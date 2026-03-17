63c1afb5c54c480aba0a5c6cfbb6c3c88f08515c
Ravindu Ishan <bkravidu@gmail.com>
2026-03-17 13:30:10 +0530

    added kafka to architecture

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -210,3 +210,32 @@ resource "aws_iam_role_policy" "github_actions_deploy" {
     ]
   })
 }
+
+# ----------------------------------------------------------------
+# Kafka node
+# ----------------------------------------------------------------
+resource "aws_eks_node_group" "kafka" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.name_prefix}-kafka"
+  node_role_arn   = aws_iam_role.eks_node.arn
+  subnet_ids      = var.private_subnet_ids
+  instance_types  = ["t3.small"]
+
+  scaling_config {
+    desired_size = 1
+    min_size     = 0
+    max_size     = 1
+  }
+
+  labels = {
+    nodegroup = "kafka"
+  }
+
+  taint {
+    key    = "workload"
+    value  = "kafka"
+    effect = "NO_SCHEDULE"
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_node_policy]
+}
