aca113b2e5e006452dbad07f80c3d946f55eae2e
Ravindu Ishan <bkravidu@gmail.com>
2026-04-25 02:03:23 +0530

    fix: resolve recipient email via User Service instead of expecting it in Kafka payload

--- OTSS_Notification_Service/src/notifications/notifications.module.ts
+++ OTSS_Notification_Service/src/notifications/notifications.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EmailModule } from '../email/email.module';
+import { HttpClientsModule } from '../http-clients/http-clients.module';
 import { KafkaConsumerService } from '../kafka/kafka-consumer.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
 @Module({
-  imports: [PrismaModule, EmailModule, ConfigModule],
+  imports: [PrismaModule, EmailModule, ConfigModule, HttpClientsModule],
   controllers: [NotificationsController],
   providers: [NotificationsService, KafkaConsumerService],
 })
